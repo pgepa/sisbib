48df23136719cbe2ba91293b4e38c6635e0462cf
cleytonmuto <cleytonmuto@gmail.com>
2022-05-24 22:33:09 -0300

    fix: disable exhaustive deps

--- src/components/Usuarios.js
+++ src/components/Usuarios.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line react-hooks/exhaustive-deps
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useMemo } from 'react';
 import UsuarioService from '../services/usuario.service';
 import { useTable } from 'react-table';
