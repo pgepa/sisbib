89582fe07c56949b56d322616fb0b4a6dbae1f34
abudluan <abudluan@gmail.com>
2022-05-26 12:50:21 -0300

    feat: navbar responsiva

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,24 +10,24 @@ import {FaUserPlus} from 'react-icons/fa';
 const NavigationBar = () => {
   return (
         <Navbar bg="success" variant="dark" expand="lg">
-    <Container fluid>
+    <Container>
         <Navbar.Brand as={Link} to="/">
-            <img src={brasao} width="50%" height="50%" alt="brasao.png" />
+            <img src={brasao} width="40%" height="40%" alt="brasao.png" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto font-size">
+        <Nav className="me-auto">
             <Nav.Link as={Link} to="/">
                 Inicio
-            </Nav.Link>
+                </Nav.Link>
             <Nav.Link as={Link} to="/obras">
                 <FaBook/> Obras
-            </Nav.Link>
+                </Nav.Link>
             <Nav.Link as={Link} to="/usuarios">
                 <FaUserPlus/> Usuarios
-            </Nav.Link>
+                </Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item as={Link} to="/usuarios">Usuários</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
             <NavDropdown.Divider />
