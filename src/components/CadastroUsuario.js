61a76c1950846312d2fa3f43757deb3767162e87
abudluan <abudluan@gmail.com>
2022-06-02 09:49:31 -0300

    refactor: tela de login e cadastro de usuário

--- src/components/CadastroUsuario.js
+++ src/components/CadastroUsuario.js
@@ -66,51 +66,51 @@ const CadastroUsuario = (props) => {
         {({ resetForm }) => (
         <Form className="container card card-usuario my-3">
           <FormGroup>
-            <FormLabel className="h4 my-2">Nome</FormLabel>
-            <Field name="name" type="text" size="lg" className="form-control shadow h4 mx-1 mb-2" />
+            <FormLabel className="h4 my-2 ">Nome</FormLabel>
+            <Field name="name" type="text" size="lg" className="form-control shadow h4 mx-0 mb-2" />
             <ErrorMessage name="name" component="div" className="text-danger" />
           </FormGroup>
           <FormGroup>
             <FormLabel className="h4 my-2">Email</FormLabel>
-            <Field name="email" type="email" size="lg" className="form-control shadow h4 mx-1 mb-2" />
+            <Field name="email" type="email" size="lg" className="form-control shadow h4 mx-0 mb-2" />
             <ErrorMessage name="email" component="div" className="text-danger" />
           </FormGroup>
           <FormGroup>
             <FormLabel className="h4 my-2">CPF</FormLabel>
-            <Field name="cpf" type="text" size="lg" className="form-control shadow h4 mx-1 mb-2" />
+            <Field name="cpf" type="text" size="lg" className="form-control shadow h4 mx-0 mb-2" />
             <ErrorMessage name="cpf" component="div" className="text-danger" />
           </FormGroup>
           <FormGroup>
             <FormLabel className="h4 my-2">Matrícula</FormLabel>
-            <Field name="inscription" type="text" size="lg" className="form-control shadow h4 mx-1 mb-2" />
+            <Field name="inscription" type="text" size="lg" className="form-control shadow h4 mx-0 mb-2" />
             <ErrorMessage name="inscription" component="div" className="text-danger" />
           </FormGroup>
           <FormGroup>
             <FormLabel className="h4 my-2">Setor</FormLabel>
-            <Field name="department" type="text" size="lg" className="form-control shadow h4 mx-1 mb-2" />
+            <Field name="department" type="text" size="lg" className="form-control shadow h4 mx-0 mb-2" />
             <ErrorMessage name="department" component="div" className="text-danger" />
           </FormGroup>
           <FormGroup>
             <FormLabel className="h4 my-2">Senha</FormLabel>
-            <Field name="password" type="password" size="lg" className="form-control shadow h4 mx-1 mb-2" />
+            <Field name="password" type="password" size="lg" className="form-control shadow h4 mx-0 mb-2" />
             <ErrorMessage name="password" component="div" className="text-danger" />
           </FormGroup>
           <FormGroup>
             <FormLabel className="h4 my-2">Confirmar senha</FormLabel>
-            <Field name="confirmPassword" type="password" size="lg" className="form-control shadow h4 mx-1 mb-2" />
+            <Field name="confirmPassword" type="password" size="lg" className="form-control shadow h4 mx-0 mb-2" />
             <ErrorMessage name="confirmPassword" component="div" className="text-danger" />
           </FormGroup>
-          <Row className="mt-4">
+          <Row className="mt-4 mx-0">
             <Col>
-              <FormGroup className="text-right ps-4">
+              <FormGroup className="text-center">
                 <Button type="submit" className="btn btn-primary rounded-pill shadow-lg px-4 py-3">
                   <BsCheckLg /><span className="mx-2">REGISTRAR</span>
                 </Button>
               </FormGroup>
             </Col>
-            <Col>
-              <FormGroup>
-                <Button type="button" onClick={() => navigate(-1)} className="btn btn-danger rounded-pill shadow-lg text-right px-4 py-3">
+            <Col>  
+              <FormGroup className="text-center">
+                <Button type="button" onClick={() => navigate(-1)} className="btn btn-danger rounded-pill shadow-lg px-4 py-3">
                   <BsXLg /><span className="mx-2">CANCELAR</span>
                 </Button>
               </FormGroup>
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { FormGroup, FormLabel } from 'react-bootstrap';
+import { FormGroup, FormLabel, Col, Row } from 'react-bootstrap';
 import { BsCheckLg, BsXLg } from 'react-icons/bs';
 import UsuariosService from '../services/usuarios.service';
 import * as Yup from 'yup';
@@ -42,23 +42,30 @@ const LoginUsuario = (props) => {
           <Form className="container card card-usuario my-3">
             <FormGroup>
               <FormLabel className="h4 my-2">Email</FormLabel>
-              <Field name="email" type="email" size="lg" className="form-control shadow h4 mx-1 mb-2" />
+              <Field name="email" type="email" size="lg" className="form-control shadow h4 mx-0 mb-2" />
               <ErrorMessage name="email" component="div" className="text-danger" />
             </FormGroup>
-            
             <FormGroup>
               <FormLabel className="h4 my-2">Senha</FormLabel>
-              <Field name="password" type="password" size="lg" className="form-control shadow h4 mx-1 mb-2" />
+              <Field name="password" type="password" size="lg" className="form-control shadow h4 mx-0 mb-2" />
               <ErrorMessage name="password" component="div" className="text-danger" />
             </FormGroup>
-            <FormGroup>
-              <Button type="submit" onClick={handleSubmit} className="btn btn-login btn-success rounded-pill shadow-lg px-4 py-3">
-                <BsCheckLg /><span className="mx-2">Entrar</span>
-              </Button>
-              <Button type="button" onClick={resetForm} className="btn btn-login btn-danger rounded-pill shadow-lg px-4 py-3">
-                <BsXLg /><span className="mx-2">CANCELAR</span>
-              </Button>
-            </FormGroup>
+            <Row className="mt-4 mx-0">
+              <Col>
+                <FormGroup className="text-center">
+                  <Button type="submit" onClick={handleSubmit} className="btn btn-primary rounded-pill shadow-lg px-4 py-3">
+                    <BsCheckLg /><span className="mx-2">Entrar</span>
+                  </Button>
+                </FormGroup>
+              </Col>
+              <Col>
+                <FormGroup className="text-center">      
+                  <Button type="button" onClick={resetForm} className="btn btn-danger rounded-pill shadow-lg px-4 py-3">
+                    <BsXLg /><span className="mx-2">CANCELAR</span>
+                  </Button>
+                </FormGroup>
+              </Col>
+            </Row>
           </Form>
         )}
       </Formik>
