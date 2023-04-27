9c2bf0c1a935afad4dcc7dbda21534860ad4beaf
abudluan <abudluan@gmail.com>
2023-04-27 13:36:27 -0300

    refact: navbar align middle

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,23 +2,24 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import brasao from '../brasao.png';
-import { FaHome, FaUsers, FaExchangeAlt } from 'react-icons/fa';
+import { FaHome, FaUsers, FaExchangeAlt} from 'react-icons/fa';
 import { BiLogIn, BiLogOut } from 'react-icons/bi';
 import AuthService from '../services/auth.service';
 import { useNavigate } from 'react-router-dom';
+import './Styles/NavigationBar.scss';
 
 const NavigationBar = (props) => {
-  const [showAdmin, setShowAdmin] = useState(false);
+  const [showAdmin, setShowAdmin] = useState(true);
   const [currentUser, setCurrentUser] = useState(null);
-  
+
   useEffect(() => {
     const user = AuthService.getCurrentUser();
     if (user) {
       setCurrentUser(user);
       setShowAdmin(user.roles.includes('ROLE_ADMIN'));
     }
-  },[]);
-  
+  }, []);
+
   const navigate = useNavigate();
 
   const logout = () => {
@@ -30,37 +31,41 @@ const NavigationBar = (props) => {
   }
 
   return (
-    <Navbar bg="success" variant="dark" expand="lg">
+    <Navbar className='navigationBar navbar-expand-lg' expand="lg">
       <Container fluid>
-        <Navbar.Brand as={Link} to="/">
-          <img src={brasao} width="50%" height="50%" alt="brasao.png" />
-        </Navbar.Brand>
+
+        <img src={brasao} className='brasao' alt="brasao.png" />
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto font-size">
-            <Nav.Link as={Link} to="/" variant="white" className="mx-4">
-              <FaHome />&nbsp;&nbsp;Início
+          <Nav className="mx-auto font-size">
+
+            <Nav.Link as={Link} to="/" >
+              <FaHome /> <span className="align-middle">Início</span>
             </Nav.Link>
-            <NavDropdown title="Obras" variant="white" className="mx-4">
-              <NavDropdown.Item as={Link} to="/obrasdetalhadas">Detalhadas</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/obrasresumidas">Resumidas</NavDropdown.Item>
+            
+            <NavDropdown className='dropdown' title="Obras">
+              <NavDropdown.Item as={Link}  to="/obrasdetalhadas" className='dropdown-item' >Detalhadas</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/obrasresumidas" className='dropdown-item' >Resumidas</NavDropdown.Item>
             </NavDropdown>
+
             {!currentUser && (
-              <Nav.Link as={Link} to="/login" variant="white" className="mx-4" >
-                <BiLogIn />&nbsp;&nbsp;Login
+              <Nav.Link as={Link} to="/login" >
+                <BiLogIn /> <span className="align-middle">Login</span>
               </Nav.Link>)}
             {showAdmin && (
-              <Nav.Link as={Link} to="/usuarios" variant="white" className="mx-4" >
-                <FaUsers />&nbsp;&nbsp;Usuários
+              <Nav.Link as={Link} to="/usuarios" >
+                <FaUsers /> <span className="align-middle">Usuários</span>
               </Nav.Link>)}
             {showAdmin && (
-              <Nav.Link as={Link} to="/emprestimos" variant="white" className="mx-4" >
-                <FaExchangeAlt />&nbsp;&nbsp;Empréstimos
+              <Nav.Link as={Link} to="/emprestimos" >
+                <FaExchangeAlt /> <span className="align-middle">Empréstimos</span>
               </Nav.Link>)}
             {currentUser && (
-              <Nav.Link  onClick={logout} variant="white" className="mx-4" >
-                <BiLogOut />&nbsp;&nbsp;Logout
+              <Nav.Link onClick={logout} >
+                <BiLogOut /> <span className="align-middle">Logout</span>
               </Nav.Link>)}
+
           </Nav>
         </Navbar.Collapse>
       </Container>
